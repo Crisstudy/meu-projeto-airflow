abd513a5279eaf86ff834a85a53ab1cc7af0ef10
CRISTINA S. FIGUEIREDO <kristinasaraiva12@gmail.com>
2026-04-11 18:31:46 +0000

    criando dags xcom

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,2 +1,35 @@
+from datatime import datetime
+from airflow import DAG
+import pandas as pd
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 AIRFLOW_HOME = '/workspaces/meu-projeto-airflow'
+
+def read_players():
+    pghook = PostgresHook(postgres_conn_id='postgres_default')
+    pghook.copy_expert(
+        sql="COPY (SELECT * FROM players) TO stdout WITH CSV HEADER",
+        filename=AIRFLOW_HOME + '/data/players.csv'
+    )
+
+    def read_currency():
+    pghook = PostgresHook(postgres_conn_id='postgres_default')
+    pghook.copy_expert(
+        sql="COPY (SELECT * FROM currency) TO stdout WITH CSV HEADER",
+        filename=AIRFLOW_HOME + '/data/currency.csv'
+    )
+
+def read_players_ids(ti):
+    df = pd.read_csv(AIRFLOW_HOME + '/data/players.csv')
+    list_ids = df['player_id'].tolist()
+    ids = str(list_ids).replace('[','(').replace(']',')')
+    ti.xcom_push(key='players_ids', value='ids')
+    
+
+
+with DAG(
+    dag_id="xcom_dag_v2",
+    schedule_interval=None,
+    start_date=datetime(2020, 1, 1),
+    catchup=False
+) as dag:
