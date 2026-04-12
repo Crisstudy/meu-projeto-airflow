bf9fd51d47bc215fb5a7b56fd913bf1308b7e060
CRISTINA S. FIGUEIREDO <kristinasaraiva12@gmail.com>
2026-04-12 18:07:00 +0000

    trabalhando com hook dags para comunicação entre de variavies e fluxos lineares de execução

---
+++ dags/execution_dag.py
@@ -0,0 +1,59 @@
+
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import time
+
+def task1():
+    print('task1')
+
+def task2():
+    print('task2')
+    time.sleep(6)
+
+def task3():
+    print('task3')
+    time.sleep(6)
+
+def task4():
+    print('task4')
+    time.sleep(6)
+
+def task5():
+    print('task5')
+    time.sleep(6)
+
+with DAG(
+    dag_id="execution_dag_v2",
+    schedule_interval=None,
+    start_date=datetime(2020, 1, 1),
+    catchup=False
+) as dag:
+    
+    t1 = PythonOperator(
+        task_id='task1',
+        python_callable=task1
+    )
+
+    t2 = PythonOperator(
+        task_id='task2',
+        python_callable=task2
+    )
+
+    t3 = PythonOperator(
+        task_id='task3',
+        python_callable=task3
+    )
+
+    t4 = PythonOperator(
+        task_id='task4',
+        python_callable=task4
+    )
+
+    t5 = PythonOperator(
+        task_id='task5',
+        python_callable=task5
+    )
+
+    t1 >> [t2, t3, t4] >> t5
--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,7 +1,9 @@
-from datatime import datetime
+from datetime import datetime
 from airflow import DAG
 import pandas as pd
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 AIRFLOW_HOME = '/workspaces/meu-projeto-airflow'
 
@@ -12,18 +14,25 @@ def read_players():
         filename=AIRFLOW_HOME + '/data/players.csv'
     )
 
-    def read_currency():
+def read_currency():
     pghook = PostgresHook(postgres_conn_id='postgres_default')
     pghook.copy_expert(
         sql="COPY (SELECT * FROM currency) TO stdout WITH CSV HEADER",
         filename=AIRFLOW_HOME + '/data/currency.csv'
     )
 
+def read_currency_modified():
+    pghook = PostgresHook(postgres_conn_id='postgres_default')
+    pghook.copy_expert(
+        sql="COPY (SELECT * FROM currency) TO stdout WITH CSV HEADER",
+        filename=AIRFLOW_HOME + '/data/currency_modified.csv'
+    )
+
 def read_players_ids(ti):
     df = pd.read_csv(AIRFLOW_HOME + '/data/players.csv')
-    list_ids = df['player_id'].tolist()
+    list_ids = df['id'].tolist()
     ids = str(list_ids).replace('[','(').replace(']',')')
-    ti.xcom_push(key='players_ids', value='ids')
+    ti.xcom_push(key='players_ids', value=ids)
     
 
 
@@ -33,3 +42,33 @@ with DAG(
     start_date=datetime(2020, 1, 1),
     catchup=False
 ) as dag:
+    
+    task1 = PythonOperator(
+        task_id='read_players',
+        python_callable=read_players
+    )
+
+    task2 = PythonOperator(
+        task_id='read_currency',
+        python_callable=read_currency
+    )
+
+    task3 = PythonOperator(
+        task_id='read_playerid',
+        python_callable=read_players_ids
+    )
+
+
+    task4 = PythonOperator(
+        task_id='read_currency_modified',
+        python_callable=read_currency_modified
+    )
+
+    task5 = PostgresOperator(
+        task_id='update_currency',
+        postgres_conn_id='postgres_default',
+        sql="UPDATE currency SET currency_amount= currency_amount + 500 WHERE player_id in"
+            "{{ task_instance.xcom_pull(task_ids='read_playerid', key='players_ids') }}"
+    )
+
+    task1 >> [task2, task3] >> task4 >> task5
